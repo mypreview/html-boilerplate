03ca57776c879283b8993307f13335fbf3e61bb7
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-10-05 14:33:28 +0300

    Creates a zip file for the project
    Updated node packages

---
+++ .plugin-zip.js
@@ -0,0 +1,42 @@
+/**
+ * External dependencies
+ */
+const AdmZip = require( 'adm-zip' );
+const { realpathSync } = require( 'fs' );
+const { sync: glob } = require( 'fast-glob' );
+const { sync: readPkgUp } = require( 'read-pkg-up' );
+const { dirname } = require( 'path' );
+const { stdout } = require( 'process' );
+
+/**
+ * Internal dependencies
+ */
+const { packageJson } = readPkgUp( {
+	cwd: realpathSync( process.cwd() ),
+} );
+const getPackageProp = ( prop ) => packageJson && packageJson[ prop ];
+const hasPackageProp = ( prop ) =>
+	packageJson && packageJson.hasOwnProperty( prop );
+const name = getPackageProp( 'name' );
+
+stdout.write( `Creating archive for \`${ name }\` project…\n\n` );
+const zip = new AdmZip();
+
+if ( hasPackageProp( 'files' ) ) {
+	stdout.write(
+		'Using the `files` field from `package.json` to detect files:\n\n'
+	);
+	const files = glob( getPackageProp( 'files' ), {
+		caseSensitiveMatch: false,
+	} );
+	files.forEach( ( file ) => {
+		stdout.write( `  Adding \`${ file }\`.\n` );
+		const zipDirectory = dirname( file );
+		zip.addLocalFile( file, zipDirectory !== '.' ? zipDirectory : '' );
+	} );
+
+	zip.writeZip( `./${ name }.zip` );
+	stdout.write( `\nDone. \`${ name }.zip\` is ready! 🎉\n` );
+} else {
+	stdout.write( '\nThe `files` field is not found in the `package.json`!\n' );
+}
