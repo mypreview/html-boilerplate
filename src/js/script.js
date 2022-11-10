be22790ae270d09d8d8d1a944a3b2166dd12ea3a
Sajedeh Gooklani <gookalani@mail.com>
2022-11-10 14:18:38 +0300

    Add `currentYear`

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
-( function () {
+( function ( $ ) {
 	( 'use strict' );
-} )();
+
+	var $currentYear = $( '#current-year' );
+	if ( $currentYear ) {
+		$currentYear.text( new Date().getFullYear() );
+	}
+} )( jQuery );
